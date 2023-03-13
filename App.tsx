230378d106519aa950a3ff2a808aa2e15eb62694
walid hamdi <walidhamdidev@gmail.com>
2023-03-13 10:15:37 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Alert, Text, View, Button, Pressable } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>My App</Text>
+      <Pressable
+        onPress={() => {
+          Alert.alert("Show Message", "Message description", [
+            { text: "Ok", onPress: () => console.log("Welcome") },
+            { text: "Bye", onPress: () => console.log("Bye") },
+          ]);
+        }}
+        style={styles.button}
+      >
+        <Text style={styles.pressableText}>Show</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  button: {
+    marginTop: 10,
+    backgroundColor: "blue",
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  pressableText: {
+    color: "white",
   },
 });
